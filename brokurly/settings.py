7d69f9c89b2078db2f67042c8a44c434fd547ed6
Dapane-dev <daphned@naver.com>
2020-10-19 17:39:40 +0900

    SECERT 키 숨김 DB migrate urls admin 삭제

--- brokurly/settings.py
+++ brokurly/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a5qhx5dcd=^ebwcyb^t$#m#9q^b+2bnnk(29)idnd30ee736_$'
+SECRET_KEY = my_settings.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
