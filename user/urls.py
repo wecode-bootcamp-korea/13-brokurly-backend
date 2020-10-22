9ef88f0488e99a568a9e70c5fc94591c08fe023c
hukim <kho5420@gmail.com>
2020-10-22 11:43:08 +0900

    Update : user/views.py 내용 삭제

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
 
-from user.views  import SignUp, SignIn, CheckEmail, CheckID
-
 urlpatterns = [
-    path('signup', SignUp.as_view()),
-    path('signin', SignIn.as_view()),
-    path('checkid', CheckID.as_view()),
-    path('checkemail', CheckEmail.as_view()),
 ]
