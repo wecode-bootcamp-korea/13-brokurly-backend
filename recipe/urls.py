ec214f9c25b264926483f355589ebbc00702be7f
hukim <kho5420@gmail.com>
2020-10-22 11:40:33 +0900

    Update : views.py 회원가입 KEY값 수정

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,9 +1,4 @@
 from django.urls  import path
 
-from recipe.views import RegisterRecipe, ViewRecipeCategory, ViewRecipeDetail
-
 urlpatterns = [
-    path('register_recipe', RegisterRecipe.as_view()),
-    path('view_recipe_category', ViewRecipeCategory.as_view()),
-    path('view_recipe_detail', ViewRecipeDetail.as_view()),
 ]
