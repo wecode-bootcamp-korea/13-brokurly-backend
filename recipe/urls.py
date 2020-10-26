629f4072f500460493693dd6a9f6a1d9b0b53087
hukim <kho5420@gmail.com>
2020-10-26 15:50:20 +0900

    Update : urls.py 컨벤션 수정

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,6 +3,6 @@ from django.urls  import path
 from recipe.views import RecipeView, RecipeDetailView
 
 urlpatterns = [
-    path('category', RecipeView.as_view()),
-    path('detail', RecipeDetailView.as_view()),
+    path('/category', RecipeView.as_view()),
+    path('/detail', RecipeDetailView.as_view()),
 ]
